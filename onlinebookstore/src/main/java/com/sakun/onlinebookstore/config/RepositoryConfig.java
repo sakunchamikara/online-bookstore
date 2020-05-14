b0d115cee90f53f2aeea01e8962b9ccdc60bfd29
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-05-14 11:09:46 +0530

    make changes in RepositoryConfig.java

---
+++ onlinebookstore/src/main/java/com/sakun/onlinebookstore/config/RepositoryConfig.java
@@ -0,0 +1,29 @@
+package com.sakun.onlinebookstore.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+//import com.sakun.onlinebookstore.entity.Book;
+//import com.sakun.onlinebookstore.entity.BookCategory;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer{
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+//		config.exposeIdsFor(Book.class);
+//		config.exposeIdsFor(BookCategory.class);
+		
+		config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType)
+				.toArray(Class[]::new));
+	}
+}
--- onlinebookstore/src/main/java/com/sakun/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/com/sakun/onlinebookstore/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package com.sakun.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.sakun.onlinebookstore.entity.BookCategory;
 
+@RepositoryRestResource(collectionResourceRel="bookCategory",path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory,Long>{
 
 }
